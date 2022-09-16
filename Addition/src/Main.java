13888c82491c731132f4d4662e367498acf74339
Aasim <asimjazah8@gmail.com>
2022-09-16 13:47:29 +0000

    First Commit

---
+++ Addition/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	int a=2;
+	int b=3;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Main obj = new Main();
+		int c= obj.a +obj.b;
+		
+		System.out.println("result is = "+c);
+				
+		
+
+	}
+
+}
